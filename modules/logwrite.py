58dfc330ad6bd64160e03185daa00fe694a5c10a
user <BUND-development@protonmail.com>
2019-11-25 00:56:26 +0300

    hotfix

--- modules/logwrite.py
+++ modules/logwrite.py
@@ -5,8 +5,13 @@
 def log(error, module, **kwargs):
 	with open("BUGREPORT", mode="a", encoding="UTF-8") as file:
 		params ="\n"
-		with open("settings.ini") as st:
+		try:
+			st = open("settings.ini", encoding="UTF-8")
 			settings = st.read()
+		except:
+			settings = "ERROR .ini"
+		else:
+			st.close()
 		for key in kwargs:
 			params = params + "\n" + str(key) + ":" + str(kwargs[key])
 		text_log = "\n============\nmodule: {0}\nerror:{1}\n--another params--{2}\n settings: {3}\n============\n".format(module, error, params, settings)
